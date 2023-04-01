c996d9ddffe09912d9c96eddc5e595fcf0ff0444
Rickazuo <rick3209@gmail.com>
2023-04-01 18:07:55 -0300

    update timer logic

--- index.js
+++ index.js
@@ -1,8 +1,43 @@
-// const plusButton = document.querySelector('plus-button')
+const plusButton = document.querySelector("#plus-button");
+const minusButton = document.querySelector("#minus-button");
+const seconds = document.querySelector("#seconds");
+const minutes = document.querySelector("#minutes");
 
+function plusSecond() {
+  const currentSeconds = parseInt(seconds.textContent);
+  let newSeconds = currentSeconds + 10;
 
-// export default plusMinute(){
+  if (newSeconds >= 60) {
+    const currentMinutes = parseInt(minutes.textContent);
+    const newMinutes = currentMinutes + 1;
+    minutes.textContent = newMinutes.toString().padStart(2, "0")
 
-// }
+    newSeconds = 0;
+  }
+  seconds.textContent = newSeconds.toString().padStart(2, "0")
+}
 
-// plusButton.addEventListener('click', plusMinute);
+function minusSecond() {
+  const currentSeconds = parseInt(seconds.textContent);
+  let currentMinutes = parseInt(minutes.textContent)
+
+    if(currentSeconds === 0 && currentMinutes === 0){
+        return;
+    }
+
+  let newSeconds = currentSeconds - 10;
+  seconds.textContent = newSeconds.toString().padStart(2, "0")
+
+  if (newSeconds < 0) {
+    newSeconds = 50
+
+    if (currentMinutes > 0 ){
+        currentMinutes--;
+        minutes.textContent = currentMinutes.toString().padStart(2, "0")
+    }
+  } 
+  seconds.textContent = newSeconds.toString().padStart(2, "0")
+}
+
+plusButton.addEventListener("click", plusSecond);
+minusButton.addEventListener("click", minusSecond);
